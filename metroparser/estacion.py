3f376621d6f28467a2fed8a8b96c32228dee76f5
Eduardo Acha <eduardoacha@gmail.com>
2014-07-06 17:54:22 -0400

    Creo la clase Estacion que permite extraer del horario del metro el nombre de las estaciones.

---
+++ metroparser/estacion.py
@@ -0,0 +1,48 @@
+__author__ = 'eduardo'
+# -*- coding: utf-8 -*-
+
+import csv
+
+line_elements = 7
+name_station = 3
+
+
+class Estacion():
+
+    def __init__(self):
+        self.file = 'estacion.csv'
+        self.set = set()
+        self.delimiter = '\t'
+
+    def set_file(self, file):
+        self.file = file
+
+    def set_delimeter(self, delimiter):
+        self.delimiter = delimiter
+
+    def reset(self):
+        self.set = set()
+
+    def extract(self, file):
+        open_file = open(file, 'rb')
+        csv_reader = csv.reader(open_file, delimiter=self.delimiter)
+
+        for row in csv_reader:
+            if len(row) != line_elements:
+                continue
+            self.set.add(row.pop(name_station))
+        open_file.close()
+
+    def write_to_file(self):
+        open_file = open(self.file, 'w')
+
+        for station in self.set:
+            open_file.write(station + '\n')
+
+        open_file.close()
+
+
+## Main ##
+e = Estacion()
+e.extract('horario.cvs')
+e.write_to_file()
