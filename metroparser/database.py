106d1a1a28b36798dbba1ed2582ba7c7abd9178b
Eduardo Acha <eduardoacha@gmail.com>
2014-07-09 10:52:48 -0400

    Agrega la funcionalidad para crear las nuevas tablas ESTACION y HORARIO, a partir de los csv generados.

--- metroparser/database.py
+++ metroparser/database.py
@@ -3,6 +3,9 @@ __author__ = 'eduardo'
 
 import sqlite3
 import json
+import csv
+
+line_elements = 7
 
 
 class AppDatabase():
@@ -10,17 +13,17 @@ class AppDatabase():
     def __init__(self, name='database', extension='db'):
         self.database_name = name
         self.database_extension = extension
-        self.conection = sqlite3.connect('./' + self.database_name + '.' + self.database_extension)
+        self.conection = sqlite3.connect(self.database_name + '.' + self.database_extension)
         self.conection.text_factory = str
         self.cursor = self.conection.cursor()
 
-    def create_database(self, schema='./schema.sql'):
+    def create_database(self, schema='schema.sql'):
         fd = open(schema, 'r')
         script = fd.read()
         self.cursor.executescript(script)
         fd.close()
 
-    def add_lines(self, data='./lineas.json'):
+    def add_lines(self, data='lineas.json'):
         fd = open(data, 'r')
         lineas = json.loads(fd.read())
 
@@ -29,13 +32,49 @@ class AppDatabase():
         self.conection.commit()
         fd.close()
 
-    def add_station(self, data='./estacion.csv'):
+    def add_station(self, data='horario.csv'):
         fd = open(data, 'r')
+        csv_reader = csv.reader(fd, delimiter='\t')
+        linea_id = None
 
-        for line in fd:
-            self.cursor.execute('INSERT INTO ESTACION (name, position) VALUES (?, ?)', (line.rstrip(), None))
+        for row in csv_reader:
+            if len(row) != line_elements:
+                linea_id = (self.cursor.execute('SELECT id FROM LINEA WHERE name = ?', (row[0],)).fetchone())[0]
+            else:
+                estacion_name = row[3]
+
+                if estacion_name != 'Estación':
+                    self.cursor.execute('INSERT INTO ESTACION (name, linea) VALUES ( ?, ? )',
+                                        (estacion_name, linea_id))
+
         self.conection.commit()
         fd.close()
+
+    def add_schedule(self, data):
+        type = 1
+        for file in data:
+            open_file = open(file, 'r')
+            csv_reader = csv.reader(open_file, delimiter='\t')
+            linea_id = None
+
+            for row in csv_reader:
+
+                if len(row) != line_elements:
+                    linea_id = (self.cursor.execute('SELECT id FROM LINEA WHERE name = ?', (row[0],)).fetchone())[0]
+                else:
+                    estacion = row[3]
+
+                    if estacion != 'Estación':
+                        print estacion
+                        estacion_id = (self.cursor.execute('SELECT id FROM ESTACION WHERE name = ? and linea = ?',
+                                                           (estacion, linea_id)).fetchone())[0]
+                        self.cursor.execute('INSERT INTO HORARIO (estacion, type, open, close, first_start, last_start,'
+                                            ' first_end, last_end) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+                                            (estacion_id, type, row[0], row[6], row[1], row[4], row[2], row[5]))
+
+            self.conection.commit()
+            open_file.close()
+            type += 1
 
     def close(self):
         self.conection.close()
@@ -44,4 +83,5 @@ a = AppDatabase()
 a.create_database()
 a.add_lines()
 a.add_station()
+a.add_schedule(['horario.csv', 'horario_sabado.csv', 'horario_domingo.csv'])
 a.close()
